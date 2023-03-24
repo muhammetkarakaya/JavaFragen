b634a8030a2f061eba249d6b856d94086e523523
muhammetkarakaya <karakayaatc@gmail.com>
2023-03-24 12:43:08 +0100

    ardisikartan

---
+++ src/ArrayLists/ardisikArtan.java
@@ -0,0 +1,27 @@
+package ArrayLists;
+
+import java.util.Arrays;
+
+public class ardisikArtan {
+    public class ArdısıkArtan {
+        public static void main(String[] args) {
+            //Array'in ardısık artan toplamını hesaplayın.
+            int[] sayilar = {9, 5, 8, 2, 6, 9, 2, 3, 8, 7};
+            System.out.println("Arrayimiz : "+ Arrays.toString(sayilar));
+            int a = 0; int[] arrToplam = new int[4];
+
+            int index = 1;
+
+            while (a < arrToplam.length) {
+                int toplam = 0;
+                for (int i = 0; i <= a; i++) {
+                    toplam += sayilar[index- 1 + i];
+                }
+                arrToplam[a] += toplam;
+                a++;
+                index += a;
+            }
+            System.out.println("Ardisik Toplam : "+ Arrays.toString(arrToplam));
+        }
+    }
+}
